3e3708eb8a65ebd845393f8d48233e02318238b8
Alex Paley <ajp368@cornell.edu>
2021-04-22 17:46:29 -0400

    qlearningstuff

---
+++ Engine2.py
@@ -0,0 +1,80 @@
+from Board import Board
+import random
+import BoardFunctions
+import Board
+import HeuristicFunctions
+from Engine1 import Engine1
+
+import copy
+class Engine2:
+    # This is my second iteration of an engine, it uses a heuristic function regarding one's in a row, two's in a row,
+    # and three's in a row in order to find the best move
+
+    def __init__(self):
+        nextmove = 0
+
+    def choose_a_move(self, player, b):
+        moves = BoardFunctions.legal_moves(b)
+        possibilities = []
+
+        random_move = Engine1()
+        random_testing = False
+        num_of_random_moves = 3
+        moves_played = BoardFunctions.moves_played(b)
+
+        threes = 0
+        twos = 0
+        ones = 0
+        best_move = ((0, 0), 0)
+
+        #Checks for instant win
+        for move in moves:
+            if BoardFunctions.four_in_a_row(b, move[0], move[1], player):
+               return move
+
+        #Checks for instant loss
+        for move in moves:
+            if BoardFunctions.four_in_a_row(b, move[0], move[1], 0-player):
+               return move
+
+
+        #Random moves in the opening
+        if random_testing and moves_played < num_of_random_moves:
+            best_move = []
+            #rand_move = random_move.choose_a_move(1, b)
+            #best_move = (rand_move, 100000)
+            current_moves = BoardFunctions.legal_moves(b)
+            for i in current_moves:
+                print(i)
+                if i[1] >1 and i[1] < 5:
+                    best_move.append(i)
+            best_move = (random.choice(best_move), 10000)
+
+
+
+        #Adds moves and scores to a list
+        for move in moves:
+            board_copy = copy.deepcopy(b)
+            board_copy[move[0]][move[1]] = player
+            threes += HeuristicFunctions.in_a_rows(board_copy, player, 3)
+            threes -= HeuristicFunctions.in_a_rows(board_copy, 0 - player, 3)
+            twos += HeuristicFunctions.in_a_rows(board_copy, player, 2)
+            twos -= HeuristicFunctions.in_a_rows(board_copy, 0 - player, 2)
+            ones += HeuristicFunctions.in_a_rows(board_copy, player, 1)
+            ones -= HeuristicFunctions.in_a_rows(board_copy, 0 - player, 1)
+            total_score = 100*threes + 10*twos + 1*ones
+           # print(total_score)
+            threes = 0
+            twos = 0
+            ones = 0
+            possibilities.append((move, total_score))
+
+        #Picks out the move with the highest score
+        for pos in possibilities:
+            if pos[1] > best_move[1]:
+                best_move = pos
+
+
+
+        return best_move[0]
+
---
+++ Engine4.py
@@ -0,0 +1,63 @@
+import BoardFunctions
+import HeuristicFunctions
+from Engine1 import Engine1
+import copy
+class Engine4:
+    # This is my first iteration of an engine, it will find a win in one move and will block a win in one move,otherwise
+    # it will play a random move
+
+    def __init__(self):
+        nextmove = 0
+
+
+    def choose_a_move(self, player, b):
+        return self.minimax(player,b)
+
+    def minimax(self, player, b):
+        move_value = self.max_value(player, b)
+        return move_value
+
+    def max_value(self, player, b):
+        moves = BoardFunctions.legal_moves(b)
+        best_move = ((0,0), -100001)
+        move_value = 0
+        for move in moves:
+            if BoardFunctions.four_in_a_row(b, move[0], move[1], player):
+                return move
+            board_copy = copy.deepcopy(b)
+            board_copy[move[0]][move[1]] = player
+            move_value = self.min_value(player, board_copy)
+            if move_value[1] > best_move[1]:
+                best_move = (move, move_value[1])
+
+        print(best_move[1])
+        return best_move[0]
+
+    def min_value(self, player, b):
+        moves = BoardFunctions.legal_moves(b)
+        best_move = ((0,0), 100001)
+        for move in moves:
+            if BoardFunctions.four_in_a_row(b, move[0], move[1], 0-player):
+                return (move, -100000)
+            board_copy = copy.deepcopy(b)
+            board_copy[move[0]][move[1]] = 0 - player
+            move_value = self.get_value(player, board_copy, move)
+            if(move_value < best_move[1]):
+                best_move = (move, move_value)
+
+        return best_move
+
+
+    def get_value(self, player, b, move):
+
+        threes = 0
+        twos = 0
+        ones = 0
+        threes += HeuristicFunctions.in_a_rows(b, player, 3)
+        threes -= HeuristicFunctions.in_a_rows(b, 0 - player, 3)
+        twos += HeuristicFunctions.in_a_rows(b, player, 2)
+        twos -= HeuristicFunctions.in_a_rows(b, 0 - player, 2)
+        ones += HeuristicFunctions.in_a_rows(b, player, 1)
+        ones -= HeuristicFunctions.in_a_rows(b, 0 - player, 1)
+        total_score = 100*threes + 10*twos + 1*ones
+        return total_score
